9cfe239eaf2ead188a5af7edd76b952de1bdabab
Duplicited <136393370+BPichen@users.noreply.github.com>
2023-06-20 14:20:27 -0500

    Bug fixes
    Fixed a bug where wei value wasn't comparing correctly causing non-valued transactions to be added to the database.
    Added block id as a database field for lesser API calls during the loop. The most recent block id is now added to the web3 filter versus starting at 0 each time so that significantly less total calls are made.
    Compressed code so that it is easier to read
    Added TX link to Telegram message

--- PinksaleBuyBot/presalebot.py
+++ PinksaleBuyBot/presalebot.py
@@ -7,12 +7,13 @@ from hexbytes import HexBytes
 cc_url = constants.cc_url
 
 telebot_key = constants.telebot_key
+chat = constants.chat
 admin_chat = constants.admin_chat
 bot = telebot.TeleBot(telebot_key)
 
 rpc = "https://mainnet.infura.io/v3/1ed399d5402048f4bc11240e9d552b9f"
 web3 = Web3(Web3.HTTPProvider(rpc))
-address = "0xa13fD4a3135B77f8924941Ce89e8897b5d53C49b"
+address = "0x06a34DE65130d950bc517aCe322ca1bC361a5b7C"
 token_abi = [{"constant":"true","inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":"false","stateMutability":"view","type":"function"},{"constant":"false","inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"constant":"true","inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":"false","stateMutability":"view","type":"function"},{"constant":"false","inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"constant":"true","inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":"false","stateMutability":"view","type":"function"},{"constant":"true","inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":"false","stateMutability":"view","type":"function"},{"constant":"true","inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":"false","stateMutability":"view","type":"function"},{"constant":"false","inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"constant":"true","inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":"false","stateMutability":"view","type":"function"},{"payable":"true","stateMutability":"payable","type":"fallback"},{"anonymous":"false","inputs":[{"indexed":"true","name":"owner","type":"address"},{"indexed":"true","name":"spender","type":"address"},{"indexed":"false","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":"false","inputs":[{"indexed":"true","name":"from","type":"address"},{"indexed":"true","name":"to","type":"address"},{"indexed":"false","name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]
 
 contract = web3.eth.contract(address=address,abi=token_abi)
@@ -38,11 +39,11 @@ def connectToDB():
         print("Error connecting to DB")
     return conn
 
-def pushToDB(tx_id):
+def pushToDB(tx_id, block):
     conn = connectToDB()
     cursor = conn.cursor()
     try:
-        query = 'INSERT INTO txs (tx_id) VALUES ("{}")'.format(tx_id)
+        query = 'INSERT INTO txs (tx_id, block) VALUES ("{}", "{}")'.format(tx_id, block)
         cursor.execute(query)
         conn.commit()
         cursor.close()
@@ -62,11 +63,20 @@ def checkDB(tx_id):
     data = cursor.fetchall()
 
     if data:
-        return True
-    
+        return True   
     else:
         return False
     
+def getLastRecord():
+    conn = connectToDB()
+    cursor = conn.cursor()
+
+    query = 'select * from txs ORDER BY id DESC LIMIT 1;'
+
+    cursor.execute(query)
+    data = cursor.fetchall()
+    
+    return data
 
 def selectAll():
     conn = connectToDB()
@@ -79,14 +89,18 @@ def selectAll():
     return data
 
 def main():
+
+    fromBlock = 0
     
     while True:
 
+        newestPost = getLastRecord()
+        if newestPost:
+            id, hash, fromBlock = newestPost[0]
         try:
             filter = web3.eth.filter({
-                'fromBlock': 17517379,
-                'address': address
-                
+                'fromBlock': fromBlock,
+                'address': address          
             })
 
             entries = filter.get_all_entries()
@@ -95,44 +109,41 @@ def main():
             response_dict = json.loads(response.text)
             price = response_dict['USD']
 
-            data = selectAll()
-
-            #print(tx_hash)
-            #pushToDB(tx_hash)
-            #pushToDB(1)
-
             for entry in entries:
                 if checkDB(entry['transactionHash'].hex()):
                     print("Found!")
-                    continue
-                else:
-                    print("Not found! Adding!")
-                    data = selectAll()
-                    pushToDB(entry['transactionHash'].hex())
-                    emoji_string = ''
-                    tx = web3.eth.get_transaction(entry['transactionHash'])
-                    if tx['value'] != '0':
-                        value = tx['value'] / (10 ** 18)
+                else:   
+                    tx_hash = entry['transactionHash'].hex()           
+                    tx = web3.eth.get_transaction(tx_hash)
+                    wei = tx['value']
+                    if wei != 0:
+                        pushToDB(tx_hash, entry['blockNumber'])
+                        data = selectAll()
+                        
+                        print("Not found! Adding!")                            
+                        emoji_string = ''       
+                        
+                        value = wei / (10 ** 18)
                         spent = (value * price)
-                        emoji_count = int((price * value) / 25)
+                        emoji_count = int((price * value) / 15)
 
                         for x in range(emoji_count):
                             emoji_string += ("🟢")
 
                         buyer_funds = ((web3.eth.get_balance(tx['from']) / 10 ** 18) * price)
-                        contributors = len(data) + 1
+                        contributors = len(data)
                         filled_balance = web3.eth.get_balance(address) / (10 ** 18)
 
-                        
-                        msg_text = "<b>Denarius Presale Buy!</b>\n" + emoji_string + "\n<b>Spent</b>: {:.2f} WETH (${:.2f})\n<b>Buyer Funds</b>: ${:.2f}\n<b>Total Contributors</b>: {}\n<b>Filled: </b> {:0.2f} WETH\n\n<a href='https://www.pinksale.finance/launchpad/0x8bccF40DB0154B24C382a5C43Cad100486d3be8a?chain=ETH'>Buy</a> | <a href='https://twitter.com/DenariusRoma'>Twitter</a>".format(value, spent, buyer_funds, contributors, filled_balance)
+                        msg_text = "<b>Denarius Presale Buy!</b>\n" + emoji_string + "\n<b>Spent</b>: {:.2f} WETH (${:.2f})\n<b>Buyer Funds</b>: ${:.2f}\n<b>Total Contributors</b>: {}\n<b>Filled: </b> {:.2f} WETH\n\n<a href='https://etherscan.io/tx/{}'>TX</a> | <a href='https://www.pinksale.finance/launchpad/0x8bccF40DB0154B24C382a5C43Cad100486d3be8a?chain=ETH'>Buy</a> | <a href='https://twitter.com/DenariusRoma'>Twitter</a>".format(value, spent, buyer_funds, contributors, filled_balance, tx_hash)
 
                         bot.send_photo(admin_chat, open(img,'rb'), caption=msg_text, parse_mode = ParseMode.HTML)
-        except:
-            print("Error received. Continuing")
+                        
+
+        except Exception as e:
+            print(e)
             continue
         time.sleep(30)
         print("Looping..")
-        
 
 if __name__ == "__main__":
-    main()
+   main()
